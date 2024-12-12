320e9ae8831cb4c2e6b563969844935f386436de
42ndLight <leichteed@gmail.com>
2024-12-12 17:41:35 +0300

    Basic Ops

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,20 @@
+#Values
+number1 = 10
+number2 = 5
+
+#Addition of the values
+value1 = number1 + number2 
+
+print("Addition of" ,number1, "and" ,number2 ,"is" ,value1)
+
+#Subtraction of the values
+
+value2 = number1 - number2 
+
+print("Subtraction of" ,number1, "and" ,number2 ,"is" ,value2)
+
+#Multiplication of the values
+
+value3 = number1 * number2 
+
+print("Multiplication of" ,number1, "and" ,number2 ,"is" ,value3)
