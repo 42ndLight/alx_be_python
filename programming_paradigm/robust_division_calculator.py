01bdb475b33ae9d6faffbc1a195f949da9a4fa4f
42ndLight <leichteed@gmail.com>
2025-01-18 15:11:26 +0300

    div fix 3

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,4 +5,4 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
-        return "IError: Please enter numeric values only."
+        return "Error: Please enter numeric values only."
