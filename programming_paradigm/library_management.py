d4f37345b7e9f71252da2d6bf41f7e13983c96e4
42ndLight <leichteed@gmail.com>
2025-01-19 22:13:16 +0300

    lib fix 1

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,7 +4,17 @@ class Book:
         self.author = author
         self._is_checked_out = False
     
-    
+    def check_out(self):
+        if not self._is_checked_out:
+            self._is_checked_out = True
+            return True
+        return False
+
+    def return_book(self):
+        if self._is_checked_out:
+            self._is_checked_out = False
+            return True
+        return False
 
 class Library:
     def __init__(self):
